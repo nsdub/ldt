b198131c9cf2ec56bda4b6a8490c08b8915d6729
nsdub <nathanielsokollward@gmail.com>
2014-01-29 09:46:49 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ldt::Application.config.secret_key_base = '1c5cab31fa52575ecedd50d8f28e35d34b55613d236314a9ea13be3dbe765c4980800a7fbb69145fbd33291320d615e7e5d62e875d6ad7fc8b1ee6751e6546c9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ldt::Application.config.session_store :cookie_store, key: '_ldt_session'
