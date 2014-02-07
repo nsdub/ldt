f0f2beb276c89d2392c73524b4ebb823818c194a
nsdub <nathanielsokollward@gmail.com>
2014-02-06 19:27:52 -0500

    autoplay videos

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,7 @@ class WelcomeController < ApplicationController
     @published_ideas = Idea.where("published_at is not ?", nil)
     if @published_ideas.any? 
       rand_video_id = @published_ideas.random.video_url.split('=').second
-      @youtube_link = "//www.youtube-nocookie.com/embed/" + rand_video_id + "?rel=0"
+      @youtube_link = "//www.youtube-nocookie.com/embed/" + rand_video_id + "?rel=0&autoplay=1"
     end
   end
 end
